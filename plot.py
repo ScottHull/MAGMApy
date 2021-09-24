031ec3371d73c7b8c6a1494e6905c2dcc60b1183
ScottHull <scottdhull21@gmail.com>
2021-09-24 13:42:41 -0400

    some work on plot.py

--- plot.py
+++ plot.py
@@ -7,12 +7,13 @@ from src.plots import collect_data, make_figure
 species_to_plot = ["Na", "O", "SiO", "Mg_g", "O2", "Fe", "FeO_g", "SiO2_g", "FeO_g", "MgO"]
 data = collect_data(path="reports/atmosphere_mole_fraction", x_header='mass fraction vaporized')
 
-fig = plt.figure(figsize=(16, 9))
+fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_xlabel("Mass Fraction Vaporized")
 ax.set_ylabel("log Mole Fraction")
 ax.set_ylim(-3, 0)
 for i in species_to_plot:
+    x_data = [j for j in data.keys() if j <= 0.8]
     y_data = []
     tmp = [data[j][i] for j in data.keys() if j <= 0.8]
     for j in tmp:
@@ -21,11 +22,15 @@ for i in species_to_plot:
         else:
             y_data.append(nan)
     ax.plot(
-        [j for j in data.keys() if j <= 0.8],
+        x_data,
         y_data,
         linewidth=2.0,
         label=i
     )
+    middle_index = int(len(x_data) / 2)
+    annotate_x_pos = x_data[middle_index]
+    annotate_y_pos = y_data[middle_index] + ((max(x_data) - min(x_data)) * 0.05)
+    ax.annotate(i, (annotate_x_pos, annotate_y_pos))
 ax.grid()
-ax.legend()
+# ax.legend()
 plt.show()
