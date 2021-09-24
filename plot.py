b7966463cc7601a6a9f02f80cf97db4b125e6bfb
ScottHull <scottdhull21@gmail.com>
2021-09-24 15:09:44 -0400

    some work on plot.py

--- plot.py
+++ plot.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from math import log10
 from numpy import nan
-import numpy as np
 
 from src.plots import collect_data, make_figure
 
@@ -9,15 +8,14 @@ species_to_plot = ["Na", "O", "SiO", "Mg_g", "O2", "Fe", "FeO_g", "SiO2_g", "FeO
 data = collect_data(path="reports/atmosphere_mole_fraction", x_header='mass fraction vaporized')
 
 
-def median(d):
-    d = np.sort(d)
-    n2 = int(len(d) / 2)
-    r = n2 % 2
-    if (r == 0):
-        med = d[n2]
+def median(lst):
+    lst.sort()  # Sort the list first
+    if len(lst) % 2 == 0:  # Checking if the length is even
+        # Applying formula which is sum of middle two divided by 2
+        return (lst[len(lst) // 2] + lst[(len(lst) - 1) // 2]) / 2
     else:
-        med = (d[n2] + data[m + 1]) / 2
-    return med
+        # If length is odd then get middle value
+        return lst[len(lst) // 2]
 
 
 def get_annotation_location(x_data, y_data, x_range, y_range):
@@ -26,7 +24,7 @@ def get_annotation_location(x_data, y_data, x_range, y_range):
     for i in z:
         if (x_range[0] <= i[0] <= x_range[1]) and (y_range[0] <= i[1] <= y_range[1]):
             p.append(i)
-    m = median(d=[i[0] for i in p])
+    m = median(lst=[i[0] for i in p])
     index = [i[0] for i in p].index(m)
     return p[index]
 
