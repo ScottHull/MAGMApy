7d796c67bef4adfe5daa54d74970371b470a20c3
ScottHull <scottdhull21@gmail.com>
2021-09-24 15:08:26 -0400

    some work on plot.py

--- plot.py
+++ plot.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
 from math import log10
 from numpy import nan
+import numpy as np
 
 from src.plots import collect_data, make_figure
 
 species_to_plot = ["Na", "O", "SiO", "Mg_g", "O2", "Fe", "FeO_g", "SiO2_g", "FeO_g", "MgO"]
 data = collect_data(path="reports/atmosphere_mole_fraction", x_header='mass fraction vaporized')
+
+
+def median(d):
+    d = np.sort(d)
+    n2 = int(len(d) / 2)
+    r = n2 % 2
+    if (r == 0):
+        med = d[n2]
+    else:
+        med = (d[n2] + data[m + 1]) / 2
+    return med
 
 
 def get_annotation_location(x_data, y_data, x_range, y_range):
@@ -14,9 +26,9 @@ def get_annotation_location(x_data, y_data, x_range, y_range):
     for i in z:
         if (x_range[0] <= i[0] <= x_range[1]) and (y_range[0] <= i[1] <= y_range[1]):
             p.append(i)
-    middle = int(len(p) / 2)
-    return p[middle]
-
+    m = median(d=[i[0] for i in p])
+    index = [i[0] for i in p].index(m)
+    return p[index]
 
 
 fig = plt.figure()
