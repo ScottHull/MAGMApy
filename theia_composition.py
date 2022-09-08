52000ff23c77b12204a0ab4dc3ee1171d6d8a5df
Scott Hull <ScottHull@users.noreply.github.com>
2022-09-08 12:38:42 -0400

    Beginning work on Monte Carlo

--- theia_composition.py
+++ theia_composition.py
@@ -339,18 +339,18 @@ runs = {
     },
 }
 
-for run in runs.keys():
-    temperature, vmf, theia_mass_fraction, earth_mass_fraction, disk_mass = runs[run].values()
-    to_dir = run
-    starting_composition = run_monte_carlo(initial_composition=bse_composition,
-                                                       target_composition=bsm_composition, temperature=temperature,
-                                                       vmf=vmf, full_report_path=to_dir, full_run_vmf=90.0)
+# for run in runs.keys():
+#     temperature, vmf, theia_mass_fraction, earth_mass_fraction, disk_mass = runs[run].values()
+#     to_dir = run
+#     starting_composition = run_monte_carlo(initial_composition=bse_composition,
+#                                                        target_composition=bsm_composition, temperature=temperature,
+#                                                        vmf=vmf, full_report_path=to_dir, full_run_vmf=90.0)
 
-pool = mp.Pool(20)
-pool.map(run_monte_carlo_mp, [[bse_composition, bsm_composition, runs[run]['temperature'], runs[run]['vmf'],
-                            90.0, run, 0.50] for run in runs.keys()])
-pool.close()
-pool.join()
+# pool = mp.Pool(20)
+# pool.map(run_monte_carlo_mp, [[bse_composition, bsm_composition, runs[run]['temperature'], runs[run]['vmf'],
+#                             90.0, run, 0.50] for run in runs.keys()])
+# pool.close()
+# pool.join()
 
 
 fig = plt.figure(figsize=(16, 9))
@@ -359,10 +359,13 @@ ax.set_xlabel("Oxide%")
 ax.set_ylabel("Oxide Wt. %")
 ax.set_title("Disk Bulk Composition")
 for run in runs.keys():
-    temperature, vmf, theia_mass_fraction, earth_mass_fraction, disk_mass = runs[run].values()
-    to_dir = run
-    disk_bulk_composition_metadata, disk_bulk_composition = read_composition_file(to_dir + "/starting_composition.csv")
-    ax.plot(disk_bulk_composition.keys(), disk_bulk_composition.values(), linewidth=2.0, label=to_dir)
+    try:
+        temperature, vmf, theia_mass_fraction, earth_mass_fraction, disk_mass = runs[run].values()
+        to_dir = run
+        disk_bulk_composition_metadata, disk_bulk_composition = read_composition_file(to_dir + "/starting_composition.csv")
+        ax.plot(disk_bulk_composition.keys(), disk_bulk_composition.values(), linewidth=2.0, label=to_dir)
+    except:
+        print("{} failed".format(run))
 ax.legend()
 plt.savefig("disk_bulk_composition.png")
 
@@ -372,20 +375,23 @@ ax.set_xlabel("Oxide%")
 ax.set_ylabel("Oxide Wt. %")
 ax.set_title("Theia Composition")
 for run in runs.keys():
-    temperature, vmf, theia_mass_fraction, earth_mass_fraction, disk_mass = runs[run].values()
-    to_dir = run
-    disk_bulk_composition_metadata, disk_bulk_composition = read_composition_file(to_dir + "/starting_composition.csv")
-    theia_weight_pct, theia_moles, theia_cations, theia_mg_si, theia_mg_al = get_theia_composition(
-        disk_bulk_composition, bse_composition, disk_mass, disk_mass * earth_mass_fraction / 100
-    )
-    metadata = {
-        "temperature": temperature,
-        "vmf": vmf,
-        "mg/si": theia_mg_si,
-        "mg/al": theia_mg_al,
-    }
-    write_file(data=theia_weight_pct, metadata=metadata, filename="theia_composition.csv", to_path=to_dir)
-    ax.plot(theia_weight_pct.keys(), theia_weight_pct.values(), linewidth=2.0, label=to_dir)
+    try:
+        temperature, vmf, theia_mass_fraction, earth_mass_fraction, disk_mass = runs[run].values()
+        to_dir = run
+        disk_bulk_composition_metadata, disk_bulk_composition = read_composition_file(to_dir + "/starting_composition.csv")
+        theia_weight_pct, theia_moles, theia_cations, theia_mg_si, theia_mg_al = get_theia_composition(
+            disk_bulk_composition, bse_composition, disk_mass, disk_mass * earth_mass_fraction / 100
+        )
+        metadata = {
+            "temperature": temperature,
+            "vmf": vmf,
+            "mg/si": theia_mg_si,
+            "mg/al": theia_mg_al,
+        }
+        write_file(data=theia_weight_pct, metadata=metadata, filename="theia_composition.csv", to_path=to_dir)
+        ax.plot(theia_weight_pct.keys(), theia_weight_pct.values(), linewidth=2.0, label=to_dir)
+    except:
+        print("{} failed".format(run))
 ax.legend()
 plt.savefig("theia_composition.png")
 
