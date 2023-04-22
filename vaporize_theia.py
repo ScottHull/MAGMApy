1c99869a83add6242171bfbe69c1e8ab341a355e
Scott Hull <ScottHull@users.noreply.github.com>
2023-04-22 16:18:46 -0400

    work in progress on vaporize_theia.py

--- vaporize_theia.py
+++ vaporize_theia.py
@@ -22,7 +22,10 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 plt.style.use('seaborn-colorblind')
 
 RUN_NEW_SIMULATIONS = False
-root_path = ""
+NUM_THREADS = 40
+GATHER = True
+# root_path = ""
+root_path = "C:/Users/Scott/OneDrive/Desktop/vaporize_theia/"
 # root_path = "/scratch/shull4/vaporize_theia/"
 
 if RUN_NEW_SIMULATIONS:
@@ -44,9 +47,9 @@ bse_composition = {  # Visscher and Fegley (2013)
     'ZnO': 6.7e-3,
 }
 
+oxides = [i for i in bse_composition.keys() if i != "Fe2O3"]
 lunar_bulk_compositions = pd.read_csv("data/lunar_bulk_compositions.csv", index_col="Oxide")
-favored_composition = [lunar_bulk_compositions["O'Neill 1991"].loc[oxide] for oxide in bse_composition.keys()
-                       if oxide != "Fe2O3"]
+favored_composition = [lunar_bulk_compositions["O'Neill 1991"].loc[oxide] for oxide in oxides]
 mass_moon = 7.34767309e22  # kg, mass of the moon
 
 # ============================== Define Runs ==============================
@@ -101,21 +104,31 @@ def __run(run, bse_composition, lunar_bulk_composition, recondensed, run_name, r
         f.write(str({k: v for k, v in theia_data.items() if k not in ['c', 'l', 'g', 't']}))
 
 
-all_models = []
-for run in runs:
-    run_name = run["run_name"]
-    for model in list(lunar_bulk_compositions.keys())[1:]:
-        for m in ['recondensed', 'not_recondensed']:
-            name = f"{run_name}_{model}_{m}"
-            path = f"{root_path}{run_name}_{model}_{m}"
+def get_all_models(gather=False):
+    all_models = []
+    if not gather:
+        for run in runs:
+            run_name = run["run_name"]
+            for model in list(lunar_bulk_compositions.keys())[1:]:
+                for m in ['recondensed', 'not_recondensed']:
+                    name = f"{run_name}_{model}_{m}"
+                    path = f"{root_path}{run_name}_{model}_{m}"
+                    all_models.append((name, path))
+    else:  # the model names are the directory names in the root path
+        for model in os.listdir(root_path):
+            name = model
+            path = f"{root_path}{model}/{model}"
             all_models.append((name, path))
+    return all_models
+
+all_models = get_all_models(gather=GATHER)
 
 # ============================== Run Simulations ==============================
 if RUN_NEW_SIMULATIONS:
-    with ThreadPoolExecutor(max_workers=40) as executor:
+    with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
         futures = {}
         for run in runs:
-            for model in list(lunar_bulk_compositions.keys())[1:]:
+            for model in list(lunar_bulk_compositions.keys())[1:3]:
                 lbc = {
                     oxide: lunar_bulk_compositions[model].loc[oxide] for oxide in bse_composition.keys() if
                     oxide != "Fe2O3"
@@ -140,15 +153,121 @@ theia_compositions = {}
 for model in all_models:
     ejecta_data = eval(open(model[1] + "/ejecta_composition.csv", 'r').read())
     theia_composition = eval(open(model[1] + "/theia_composition.csv", 'r').read())
-    ejecta_compositions.update({model[0]: ejecta_data})
-    theia_compositions.update({model[0]: theia_composition})
+    ejecta_compositions.update({model[0]: ejecta_data['ejecta_composition']})
+    theia_compositions.update({model[0]: theia_composition['theia_weight_pct']})
 
 # get the min and max values for each oxide
-min_max_ejecta_compositions = {}
-min_max_theia_compositions = {}
+min_max_ejecta_compositions = {'with recondensation': {oxide: [1e99, -1e99] for oxide in oxides}, 'without recondensation': {oxide: [1e99, -1e99] for oxide in oxides}}
+min_max_theia_compositions = {'with recondensation': {oxide: [1e99, -1e99] for oxide in oxides}, 'without recondensation': {oxide: [1e99, -1e99] for oxide in oxides}}
 for oxide in bse_composition.keys():
-    if oxide != "Fe2O3":
-        min_max_ejecta_compositions.update({oxide: [min([ejecta_compositions[model][oxide] for model in all_models]),
-                                                    max([ejecta_compositions[model][oxide] for model in all_models])]})
-        min_max_theia_compositions.update({oxide: [min([theia_compositions[model][oxide] for model in all_models]),
-                                                   max([theia_compositions[model][oxide] for model in all_models])]})
+    for model, path in all_models:
+        if oxide != "Fe2O3":
+            if "_not_recondensed" in model:
+                if ejecta_compositions[model][oxide] < min_max_ejecta_compositions['without recondensation'][oxide][0]:
+                    min_max_ejecta_compositions['without recondensation'][oxide][0] = ejecta_compositions[model][oxide]
+                if ejecta_compositions[model][oxide] > min_max_ejecta_compositions['without recondensation'][oxide][1]:
+                    min_max_ejecta_compositions['without recondensation'][oxide][1] = ejecta_compositions[model][oxide]
+                if theia_compositions[model][oxide] < min_max_theia_compositions['without recondensation'][oxide][0]:
+                    min_max_theia_compositions['without recondensation'][oxide][0] = theia_compositions[model][oxide]
+                if theia_compositions[model][oxide] > min_max_theia_compositions['without recondensation'][oxide][1]:
+                    min_max_theia_compositions['without recondensation'][oxide][1] = theia_compositions[model][oxide]
+            else:
+                if ejecta_compositions[model][oxide] < min_max_ejecta_compositions['with recondensation'][oxide][0]:
+                    min_max_ejecta_compositions['with recondensation'][oxide][0] = ejecta_compositions[model][oxide]
+                if ejecta_compositions[model][oxide] > min_max_ejecta_compositions['with recondensation'][oxide][1]:
+                    min_max_ejecta_compositions['with recondensation'][oxide][1] = ejecta_compositions[model][oxide]
+                if theia_compositions[model][oxide] < min_max_theia_compositions['with recondensation'][oxide][0]:
+                    min_max_theia_compositions['with recondensation'][oxide][0] = theia_compositions[model][oxide]
+                if theia_compositions[model][oxide] > min_max_theia_compositions['with recondensation'][oxide][1]:
+                    min_max_theia_compositions['with recondensation'][oxide][1] = theia_compositions[model][oxide]
+
+# ========================== PLOT THE RANGE OF EJECTA COMPOSITIONS ==========================
+fig, axs = plt.subplots(1, 2, figsize=(16, 9))
+axs = axs.flatten()
+axs[0].set_title("Without Recondensation")
+axs[1].set_title("With Recondensation")
+for ax in axs:
+    ax.grid()
+    ax.axhline(y=1, color="black", linewidth=4, alpha=1, label="BSE")
+for i, model in enumerate(all_models):
+    to_index = 1
+    min_max_ejecta_compositions_sel = min_max_ejecta_compositions['with recondensation']
+    if "not_recondensed" in model[0]:
+        to_index = 0
+        min_max_ejecta_compositions_sel = min_max_ejecta_compositions['without recondensation']
+    # shade the region between the min and max values
+    axs[to_index].fill_between(oxides,
+                               [min_max_ejecta_compositions_sel[oxide][0] / bse_composition[oxide] for oxide in oxides],
+                               [min_max_ejecta_compositions_sel[oxide][1] / bse_composition[oxide] for oxide in oxides],
+                               alpha=0.5, color='grey')
+
+axs[0].plot(
+    oxides,
+    [ejecta_compositions["Canonical Model_O'Neill 1991_not_recondensed"][oxide] /
+    bse_composition[oxide] for oxide in oxides],
+    linewidth=2.0,
+    color='blue',
+    label="O'Neill 1991 Model"
+)
+axs[1].plot(
+    oxides,
+    [ejecta_compositions["Canonical Model_O'Neill 1991_recondensed"][oxide] /
+    bse_composition[oxide] for oxide in oxides],
+    linewidth=2.0,
+    color='blue',
+    label="O'Neill 1991 Model"
+)
+axs[1].legend(loc='upper right', fontsize=16)
+
+plt.show()
+
+
+
+
+# ========================== PLOT THE RANGE OF THEIA COMPOSITIONS ==========================
+fig, axs = plt.subplots(1, 2, figsize=(16, 9))
+axs = axs.flatten()
+axs[0].set_title("Without Recondensation")
+axs[1].set_title("With Recondensation")
+axs[0].set_ylabel("Bulk Composition / BSE Composition", fontsize=16)
+for ax in axs:
+    ax.grid()
+    ax.axhline(y=1, color="black", linewidth=4, alpha=1, label="BSE")
+    # shade region red underneath y=0
+    ax.fill_between(oxides, [0 for oxide in oxides], [-1e99 for oxide in oxides], alpha=0.2, color='red')
+    ax.set_ylim(bottom=-1.0, top=4)
+    # make all font size larger
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+for i, model in enumerate(all_models):
+    to_index = 1
+    min_max_theia_compositions_sel = min_max_theia_compositions['with recondensation']
+    if "not_recondensed" in model[0]:
+        to_index = 0
+        min_max_theia_compositions_sel = min_max_theia_compositions['without recondensation']
+    # shade the region between the min and max values
+    axs[to_index].fill_between(oxides,
+                               [min_max_theia_compositions_sel[oxide][0] / bse_composition[oxide] for oxide in oxides],
+                               [min_max_theia_compositions_sel[oxide][1] / bse_composition[oxide] for oxide in oxides],
+                               alpha=0.5, color='grey')
+
+axs[0].plot(
+    oxides,
+    [theia_compositions["Canonical Model_O'Neill 1991_not_recondensed"][oxide] /
+    bse_composition[oxide] for oxide in oxides],
+    linewidth=2.0,
+    color='blue',
+    label="O'Neill 1991 Model"
+)
+axs[1].plot(
+    oxides,
+    [theia_compositions["Canonical Model_O'Neill 1991_recondensed"][oxide] /
+    bse_composition[oxide] for oxide in oxides],
+    linewidth=2.0,
+    color='blue',
+    label="O'Neill 1991 Model"
+)
+axs[1].legend(loc='upper right', fontsize=16)
+
+plt.show()
