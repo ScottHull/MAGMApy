61b55531d26872f59b1ac65b1c455a699a3e87a5
ScottHull <scottdhull21@gmail.com>
2023-06-02 00:53:32 -0400

    working on vaporize_theia.py

--- vaporize_theia.py
+++ vaporize_theia.py
@@ -602,9 +602,9 @@ for index, ax in enumerate(axs):
     )
 axs[0].set_title("Mass Loss Fraction (Without Recondensation)", fontsize=16)
 axs[1].set_title("Mass Loss Fraction (With Recondensation)", fontsize=16)
-plt.tight_layout()
 for ax in axs:
-    axs[0].set_ylabel("Mass Loss Fraction (%)", fontsize=20)
+    ax.set_ylabel("Mass Loss Fraction (%)", fontsize=20)
+plt.tight_layout()
 # axs[1].legend(loc='lower right', fontsize=14)
 # add legend to the right of the figure
 fig.legend(loc=7)
