448fc7ed12348ca5da10e0234a1391ae88e72e92
Scott Hull <ScottHull@users.noreply.github.com>
2023-06-14 10:58:01 -0400

    work in progress on vaporize_theia.py

--- vaporize_theia.py
+++ vaporize_theia.py
@@ -641,7 +641,7 @@ for i, s in enumerate(ejecta_compositions.keys()):
     run_name = run['run_name']
     vapor_loss_fraction = run['vapor_loss_fraction']
     # read in the ejecta composition file
-    mass_distribution = pd.read_csv(f"{root_path}/{s}" + "/mass_distribution.csv")
+    mass_distribution = pd.read_csv(f"{root_path}/{s}" + "/mass_distribution.csv", index_col='component')
     # get the loss fraction of each element
     vapor_fraction = {element: mass_distribution.loc['bulk vapor mass', element] / (mass_distribution.loc['melt mass', element] + mass_distribution.loc['bulk vapor mass', element]) * 100.0 for element in cations}
     # sort cations by 50% condensation temperature
