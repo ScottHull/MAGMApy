ac72393283155d928dacea3479caaa5ddc5ed20f
ScottHull <scottdhull21@gmail.com>
2023-12-11 09:55:12 -0500

    added mars.py

--- mars.py
+++ mars.py
@@ -30,11 +30,12 @@ plt.rcParams.update({"font.size": 12})
 
 runs = [
     {
-        "run_name": "",
-        "temperature": 2682.61,  # K
-        "vmf": 0.96,  # %
-        "impactor%": 0.0,  # %
-        "new_simulation": True,  # True to run a new simulation, False to load a previous simulation
+        "run_name": "H",
+        "temperature": 2201.89,  # K
+        "vmf": 0.7785655850744196,  # %
+        "impactor%": 70.758 / 100,  # %
+        "vapor_loss_fraction": 100 / 100,  # %
+        "new_simulation": False,  # True to run a new simulation, False to load a previous simulation
     },
 ]
 
@@ -64,73 +65,9 @@ d_type_asteroid_composition = normalize({
     'K2O': 0.11,
     'ZnO': 0.04,
 })
-
-for run in runs:
-    # assign dictionary values to variables
-    run_name = run["run_name"]
-    temperature = run["temperature"]
-    vmf = run["vmf"]
-    disk_theia_mass_fraction = run["disk_theia_mass_fraction"]
-    disk_mass = run["disk_mass"]
-    vapor_loss_fraction = run["vapor_loss_fraction"]
-    new_simulation = run["new_simulation"]
-
-    if new_simulation:
-        c = Composition(
-            composition=bse_composition
-        )
-
-        g = GasPressure(
-            composition=c,
-            major_gas_species=major_gas_species,
-            minor_gas_species="__all__",
-        )
-
-        l = LiquidActivity(
-            composition=c,
-            complex_species="__all__",
-            gas_system=g
-        )
-
-        t = ThermoSystem(composition=c, gas_system=g, liquid_system=l)
-
-        reports = Report(composition=c, liquid_system=l, gas_system=g, thermosystem=t, to_dir=run_name)
-
-        count = 1
-        while t.weight_fraction_vaporized < 0.9:
-            l.calculate_activities(temperature=temperature)
-            g.calculate_pressures(temperature=temperature, liquid_system=l)
-            if l.counter == 1:
-                l.calculate_activities(temperature=temperature)
-                g.calculate_pressures(temperature=temperature, liquid_system=l)
-            t.vaporize()
-            l.counter = 0  # reset Fe2O3 counter for next vaporizaiton step
-            print(
-                "[~] At iteration: {} (Magma Fraction Vaporized: {} %)".format(
-                    count, t.weight_fraction_vaporized * 100.0))
-            if count % 50 == 0 or count == 1:
-                reports.create_composition_report(iteration=count)
-                reports.create_liquid_report(iteration=count)
-                reports.create_gas_report(iteration=count)
-            count += 1
-
-def write_mass_distribution_file(melt_mass_at_vmf, bulk_vapor_mass_at_vmf, run_name,
-                                 escaping_vapor_mass_at_vmf, retained_vapor_mass_at_vmf):
-    if os.path.exists(f"{run_name}/mass_distribution.csv"):
-        os.remove(f"{run_name}/mass_distribution.csv")
-    with open(f"{run_name}/mass_distribution.csv", "w") as f:
-        header = "component," + ",".join([str(i) for i in melt_mass_at_vmf.keys()]) + "\n"
-        f.write(header)
-        f.write("melt mass," + ",".join([str(i) for i in melt_mass_at_vmf.values()]) + "\n")
-        f.write("bulk vapor mass," + ",".join([str(i) for i in bulk_vapor_mass_at_vmf.values()]) + "\n")
-        f.write("bulk system mass," + ",".join([str(i) for i in (np.array(list(melt_mass_at_vmf.values())) + np.array(
-            list(bulk_vapor_mass_at_vmf.values()))).tolist()]) + "\n")
-        f.write("escaping vapor mass," + ",".join([str(i) for i in escaping_vapor_mass_at_vmf.values()]) + "\n")
-        f.write("retained vapor mass," + ",".join([str(i) for i in retained_vapor_mass_at_vmf.values()]) + "\n")
-        f.write("recondensed melt mass," + ",".join([str(i) for i in (np.array(list(melt_mass_at_vmf.values())) + np.array(
-            list(retained_vapor_mass_at_vmf.values()))).tolist()]) + "\n")
-    print(f"wrote file {run_name}/mass_distribution.csv")
-    f.close()
+oxides_ordered = [
+    "Al2O3", "TiO2", "CaO", "MgO", "FeO", "SiO2", "K2O", "Na2O", "ZnO"
+]
 
 
 def get_composition_at_vmf(d: dict, vmf_val: float):
@@ -169,7 +106,7 @@ def recondense_vapor(melt_absolute_cation_masses: dict, vapor_absolute_cation_ma
     }
     # convert to oxide mass fractions
     c = ConvertComposition().cations_mass_to_oxides_weight_percent(
-        cations=recondensed_melt_mass, oxides=list(bse_composition.keys())
+        cations=recondensed_melt_mass, oxides=list(bulk_composition.keys())
     )
     # divide by 100 to get mass fraction
     return {
@@ -178,94 +115,6 @@ def recondense_vapor(melt_absolute_cation_masses: dict, vapor_absolute_cation_ma
         "retained_vapor_mass": retained_vapor_mass,
         "recondensed_melt_mass": recondensed_melt_mass
     }
-
-
-def get_all_data_for_runs():
-    data = {}
-    for r in runs:
-        run = r["run_name"]
-        data[run] = r
-        # get the data
-        melt_oxide_mass_fraction = collect_data(path=f"{run}/magma_oxide_mass_fraction",
-                                                x_header='mass fraction vaporized')
-        magma_element_mass = collect_data(path=f"{run}/magma_element_mass",
-                                          x_header='mass fraction vaporized')
-        vapor_species_mass = collect_data(path=f"{run}/total_vapor_species_mass",
-                                          x_header='mass fraction vaporized')
-        vapor_element_mass = collect_data(path=f"{run}/total_vapor_element_mass",
-                                          x_header='mass fraction vaporized')
-        vapor_species_mass_fraction = collect_data(path=f"{run}/total_vapor_species_mass_fraction",
-                                                   x_header='mass fraction vaporized')
-        vapor_element_mass_fraction = collect_data(path=f"{run}/total_vapor_element_mass_fraction",
-                                                   x_header='mass fraction vaporized')
-        vapor_element_mole_fraction = collect_data(path=f"{run}/atmosphere_total_mole_fraction",
-                                                   x_header='mass fraction vaporized')
-        melt_oxide_mass_fraction_at_vmf = get_composition_at_vmf(
-            d=melt_oxide_mass_fraction,
-            vmf_val=r["vmf"]
-        )
-        magma_element_mass_at_vmf = get_composition_at_vmf(
-            d=magma_element_mass,
-            vmf_val=r["vmf"]
-        )
-        vapor_species_mass_at_vmf = get_composition_at_vmf(
-            d=vapor_species_mass,
-            vmf_val=r["vmf"]
-        )
-        vapor_element_mass_at_vmf = get_composition_at_vmf(
-            d=vapor_element_mass,
-            vmf_val=r["vmf"]
-        )
-        vapor_species_mass_fraction_at_vmf = get_composition_at_vmf(
-            d=vapor_species_mass_fraction,
-            vmf_val=r["vmf"]
-        )
-        vapor_element_mass_fraction_at_vmf = get_composition_at_vmf(
-            d=vapor_element_mass_fraction,
-            vmf_val=r["vmf"]
-        )
-
-        recondensed = recondense_vapor(
-            melt_absolute_cation_masses=magma_element_mass_at_vmf,
-            vapor_absolute_cation_mass=vapor_element_mass_at_vmf,
-            vapor_loss_fraction=r["vapor_loss_fraction"]
-        )
-
-        recondensed_melt_oxide_mass_fraction = recondensed["recondensed_melt_oxide_mass_fraction"]
-        escaping_vapor_mass = recondensed["lost_vapor_mass"]
-        retained_vapor_mass = recondensed["retained_vapor_mass"]
-        recondensed_melt_mass = recondensed["recondensed_melt_mass"]
-
-        vapor_element_mole_fraction_at_vmf = get_composition_at_vmf(
-            d=vapor_element_mole_fraction,
-            vmf_val=r["vmf"]
-        )
-
-        # add each data set to the dictionary
-        data[run]["melt_oxide_mass_fraction"] = melt_oxide_mass_fraction
-        data[run]["magma_element_mass"] = magma_element_mass
-        data[run]["vapor_species_mass"] = vapor_species_mass
-        data[run]["vapor_element_mass"] = vapor_element_mass
-        data[run]["vapor_species_mass_fraction"] = vapor_species_mass_fraction
-        data[run]["vapor_element_mass_fraction"] = vapor_element_mass_fraction
-        data[run]["melt_oxide_mass_fraction_at_vmf"] = melt_oxide_mass_fraction_at_vmf
-        data[run]["magma_element_mass_at_vmf"] = magma_element_mass_at_vmf
-        data[run]["vapor_species_mass_at_vmf"] = vapor_species_mass_at_vmf
-        data[run]["vapor_element_mass_at_vmf"] = vapor_element_mass_at_vmf
-        data[run]["vapor_species_mass_fraction_at_vmf"] = vapor_species_mass_fraction_at_vmf
-        data[run]["vapor_element_mass_fraction_at_vmf"] = vapor_element_mass_fraction_at_vmf
-        data[run]["recondensed_melt_oxide_mass_fraction"] = recondensed_melt_oxide_mass_fraction
-        data[run]["vapor_element_mole_fraction"] = vapor_element_mole_fraction
-        data[run]["vapor_element_mole_fraction_at_vmf"] = vapor_element_mole_fraction_at_vmf
-
-        # write the mass distribution file
-        write_mass_distribution_file(
-            melt_mass_at_vmf=magma_element_mass_at_vmf, bulk_vapor_mass_at_vmf=vapor_element_mass_at_vmf,
-            run_name=run,
-            escaping_vapor_mass_at_vmf=escaping_vapor_mass, retained_vapor_mass_at_vmf=retained_vapor_mass
-        )
-
-    return data
 
 
 def format_species_string(species):
@@ -282,4 +131,101 @@ def format_species_string(species):
     # return "".join(species)
 
 
+def mix_target_impactor_composition(target_composition, impactor_composition, impactor_mass_fraction):
+    """
+    Mixes the target and impactor composition together.
+    :param target_composition:
+    :param impactor_composition:
+    :param impactor_mass_fraction:
+    :return:
+    """
+    target_mass_fraction = 1 - impactor_mass_fraction
+    mixed_composition = {}
+    for k, v in target_composition.items():
+        mixed_composition[k] = v * target_mass_fraction + impactor_composition[k] * impactor_mass_fraction
+    return mixed_composition
 
+
+# make a 1 column 3 row figure
+fig, axs = plt.subplots(3, 1, figsize=(8, 12), sharex='all', sharey='all', gridspec_kw=dict(hspace=0, wspace=0))
+
+for run in runs:
+    # assign dictionary values to variables
+    run_name = run["run_name"]
+    temperature = run["temperature"]
+    impactor_disk_mass_fraction = run["impactor%"]
+    new_simulation = run["new_simulation"]
+    mixed_composition = mix_target_impactor_composition(
+        target_composition=mars_composition,
+        impactor_composition=d_type_asteroid_composition,
+        impactor_mass_fraction=impactor_disk_mass_fraction
+    )
+
+    for comp_index, (comp_name, bulk_composition) in enumerate(zip(['Mars', "D-type", "Mixed"],
+                                           [mars_composition, d_type_asteroid_composition, mixed_composition])):
+
+        if new_simulation:
+            c = Composition(
+                composition=bulk_composition
+            )
+
+            g = GasPressure(
+                composition=c,
+                major_gas_species=[
+                    "SiO", "O2", "MgO", "Fe", "Ca", "Al", "Ti", "Na", "K", "Zn"
+                ],
+                minor_gas_species="__all__",
+            )
+
+            l = LiquidActivity(
+                composition=c,
+                complex_species="__all__",
+                gas_system=g
+            )
+
+            t = ThermoSystem(composition=c, gas_system=g, liquid_system=l)
+
+            reports = Report(composition=c, liquid_system=l, gas_system=g,
+                             thermosystem=t, to_dir=run_name + f" ({comp_name})")
+
+            count = 1
+            while t.weight_fraction_vaporized < 0.9:
+                l.calculate_activities(temperature=temperature)
+                g.calculate_pressures(temperature=temperature, liquid_system=l)
+                if l.counter == 1:
+                    l.calculate_activities(temperature=temperature)
+                    g.calculate_pressures(temperature=temperature, liquid_system=l)
+                t.vaporize()
+                l.counter = 0  # reset Fe2O3 counter for next vaporizaiton step
+                print(
+                    "[~] At iteration: {} (Magma Fraction Vaporized: {} %)".format(
+                        count, t.weight_fraction_vaporized * 100.0))
+                if count % 50 == 0 or count == 1:
+                    reports.create_composition_report(iteration=count)
+                    reports.create_liquid_report(iteration=count)
+                    reports.create_gas_report(iteration=count)
+                count += 1
+
+        # get the residual melt data
+        melt_oxide_as_func_vmf = collect_data(path=run_name + f" ({comp_name})/magma_oxide_mass_fraction",
+                                                x_header='mass fraction vaporized')
+        melt_oxide_at_vmf = get_composition_at_vmf(d=melt_oxide_as_func_vmf, vmf_val=run['vmf'])
+
+        axs[comp_index].plot(
+            [format_species_string(i) for i in oxides_ordered],
+            [melt_oxide_at_vmf[i] / mars_composition[i] for i in oxides_ordered],
+            linewidth=2.0,
+            label=run_name,
+        )
+
+for comp_label, ax in zip(['Mars', "D-type\nAsteroid", "Mixed"], axs.flatten()):
+    ax.text(
+        0.05, 0.9, f"{comp_label}", transform=ax.transAxes, fontweight='bold', size=20
+    )
+
+for ax in axs.flatten():
+    ax.grid()
+    ax.set_ylabel("Disk Composition / Mars Composition", fontsize=16)
+    ax.set_yscale("log")
+
+plt.show()
